b37593745fd1a0f08827026980630eb11a5ab064
thelordvampire <thelordvampire93@gmail.com>
2020-07-18 20:11:09 +0700

    update

--- src/main/java/com/example/helloworld/HelloworldApplication.java
+++ src/main/java/com/example/helloworld/HelloworldApplication.java
@@ -16,7 +16,7 @@ public class HelloworldApplication {
 	class HelloworldController {
 		@GetMapping("/")
 		String hello() {
-			return "Hello " + target + "999999999999999";
+			return "Hello " + target + "2222222222222222222a";
 		}
 	}
 
